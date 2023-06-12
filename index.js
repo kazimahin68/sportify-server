a497c8b77653fe8977da31be43981e37383c320c
kazimahin68 <kazimahin68@gmail.com>
2023-06-13 04:20:16 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+// Verify JWT
+const verifyJWT = (req, res, next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -30,31 +48,36 @@ async function run() {
 
         const userCollection = client.db("sportifyDb").collection("users");
 
-
+        // JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
         // User related API
-        app.get('/users', async(req, res) =>{
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
 
-        app.post('/users', async(req, res) =>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email : user.email}
+            const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: 'User already exists'})
+            if (existingUser) {
+                return res.send({ message: 'User already exists' })
             }
             const result = await userCollection.insertOne(user);
             res.send(result)
         })
 
-        app.patch('/users/admin/:id', async(req, res) =>{
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const update = {
                 $set: {
                     role: 'admin'
-                }, 
+                },
             }
             const result = await userCollection.updateOne(filter, update)
             res.send(result)
@@ -63,13 +86,13 @@ async function run() {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const update = {
-              $set: {
-                role: "instructor",
-              },
+                $set: {
+                    role: "instructor",
+                },
             };
             const result = await userCollection.updateOne(filter, update);
             res.send(result);
-          });
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
